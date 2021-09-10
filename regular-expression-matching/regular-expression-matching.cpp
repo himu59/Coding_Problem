a3126ed6d2a3545493f86c5823d73016e22e564f
Himanshu Negi <52888563+himu59@users.noreply.github.com>
2021-09-10 23:21:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.92%) - LeetHub

--- regular-expression-matching/regular-expression-matching.cpp
+++ regular-expression-matching/regular-expression-matching.cpp
@@ -19,18 +19,13 @@ public:
                     dp[i][j] = dp[i-1][j-1];
                 }else if(p[j-1] == '*') {
                     if(j-2>=0 and dp[i][j-2] == 1) dp[i][j] = 1;
-                    else if(j-2>=0 and (p[j-2] == s[i-1]) or p[j-2] == '.') {
+                    else if(j-2>=0 and (p[j-2] == s[i-1] or p[j-2] == '.')) {
                         dp[i][j] = dp[i][j-1] or dp[i-1][j];
                     }
                 }
             }
         }
-        for(int i=0;i<=n;i++) {
-            for(int j=0;j<=m;j++) cout<<dp[i][j]<<' ';
-            cout<<endl;
-            
-        }
-            
+                    
         return dp[n][m];
     }
 };
